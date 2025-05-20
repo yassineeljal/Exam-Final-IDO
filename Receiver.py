8e8750b6a9075ea2ff2b2b4111981f37b565c81e
Yassine El Jal <2387923@collegemv.qc.ca>
2025-05-20 10:13:11 -0400

    ajout du threading et fnalistaion du receiver

--- Receiver.py
+++ Receiver.py
@@ -1,53 +1,107 @@
 import paho.mqtt.client as pmc
 import pigpio
+import time
 
 BROKER = "mqttbroker.lan"
 PORT = 1883
-TOPIC = "final/+/T"
-TOPIC2 = "final/+/H"
-
+TOPIC_T = "final/+/T"
+TOPIC_H = "final/+/H"
 
 LED_ROUGE = 19
 LED_BLEU = 26
 LED_BLANCHE = 13
 
-
+hostname = "takfayassine" 
 pi = pigpio.pi()
-
-pi.set_mode(LED_BLANCHE, pigpio.OUTPUT)
 pi.set_mode(LED_ROUGE, pigpio.OUTPUT)
 pi.set_mode(LED_BLEU, pigpio.OUTPUT)
+pi.set_mode(LED_BLANCHE, pigpio.OUTPUT)
 
-def connexion(client, userdata, flags, code, properties):
+dictionnaire_temperature = {}
+dictionnaire_humidite = {}
+dernierEnvoiDeDonnees = 0
+
+def connexion(client, userdata, flags, code, properties=None):
     if code == 0:
-        print("Connecté")
+        print("Connecté au broker MQTT.")
+        client.subscribe(TOPIC_T)
+        client.subscribe(TOPIC_H)
     else:
-        print("Erreur code %d\n", code)
+        print(f"Erreur de connexion: code {code}")
 
-def reception_msg(cl,userdata,msg):
-    if (client.publish(TOPIC, TOPIC2)):
-        if (max((client.publish(TOPIC, "TEMPERATURE")))):
-            pi.write(LED_ROUGE, 1)
-            pi.sleep(2)
-            pi.write(LED_ROUGE, 0)
-        elif (max((client.publish(TOPIC2, "HUMIDITY")))):
-            pi.write(LED_ROUGE, 1)
-            pi.sleep(2)
-            pi.write(LED_ROUGE, 0)
+def gestionLeds():
+    if hostname in dictionnaire_temperature:
+        temperatureMax = max(dictionnaire_temperature.values())
+        if dictionnaire_temperature[hostname] == temperatureMax:
+            pi.write(LED_ROUGE, 1)  
         else:
-            print("error")
-        
-        
-        print("Reçu:",msg.payload.decode())
+            pi.write(LED_ROUGE, 0)  
+    else:
+        pi.write(LED_ROUGE, 0)  
+
+    if hostname in dictionnaire_humidite:
+        humiditeMax = max(dictionnaire_humidite.values())
+        if dictionnaire_humidite[hostname] == humiditeMax:
+            pi.write(LED_BLEU, 1) 
+        else:
+            pi.write(LED_BLEU, 0) 
+    else:
+        pi.write(LED_BLEU, 0)  
+
+
+def reception_msg(client, userdata, msg):
+    global dernierEnvoiDeDonnees
+
+    topic = msg.topic
+    payload = msg.payload.decode()
+
+    try:
+        valeur = int(payload)
+    except ValueError:
+        print(f"Valeur non entière ignorée : {payload}")
+        return
+
+    parts = topic.split('/')
+    if len(parts) != 3:
+        print(f"Format de topic inattendu : {topic}")
+        return
+
+    host = parts[1]
+    mesure = parts[2]
+
+
+    print(f"Reçu de {host} | {mesure} = {valeur}")
+
+    if mesure == "T":
+        dictionnaire_temperature[host] = valeur
+    elif mesure == "H":
+        dictionnaire_humidite[host] = valeur
+    else:
+        return
+
+    if host == hostname:
+        dernierEnvoiDeDonnees = time.time()
+
+    gestionLeds()
 
 client = pmc.Client(pmc.CallbackAPIVersion.VERSION2)
 client.on_connect = connexion
 client.on_message = reception_msg
+client.connect(BROKER, PORT)
+client.loop_start()
 
-client.connect(BROKER,PORT)
-client.subscribe(TOPIC)
-client.subscribe(TOPIC2)
-client.loop_forever()
+try:
+    while True:
+        now = time.time()
+        if now - dernierEnvoiDeDonnees <= 35:
+            pi.write(LED_BLANCHE, 1) 
+        else:
+            pi.write(LED_BLANCHE, 0)  
+        time.sleep(1)
 
-
-
+except KeyboardInterrupt:
+    print("Fin du programme.")
+    pi.write(LED_ROUGE, 0)
+    pi.write(LED_BLEU, 0)
+    pi.write(LED_BLANCHE, 0)
+    client.disconnect()
--- SendTemp.py
+++ SendTemp.py
@@ -3,6 +3,9 @@ from time import sleep
 import time
 import pigpio 
 from pigpio_dht import DHT11
+import threading
+from flask import Flask, jsonify, request
+
 
 gpio = 4
 sensor = DHT11(gpio)
@@ -37,58 +40,114 @@ HUMIDITY = str(result.get("humidity"))
 ETAT = True
 
 
-def sendData():
+
+
+
+app = Flask(__name__)
+pi = pigpio.pi()
+
+@app.route('/donnees',methods=['GET'])
+def get_temp():
+  global TEMPERATURE, HUMIDITY
+  return jsonify({
+     'T': TEMPERATURE,
+     'H': HUMIDITY
+  })
+
+@app.route('/etat', methods=['POST'])
+def set_etat():
     global ETAT
-    if(pi.read(BOUTON == 0)):
-        start = time.time()
-        while True:
-            if(pi.read(BOUTON == 1)):
-                end = time.time()
-                if(end - start >= 2):
+    if request.method == "POST":
+      json = request.get_json()
+      if "etat" in json:
+        if json["etat"] == 1:
+            ETAT = True
+        elif json["etat"] == 0:
+            ETAT = False
+        else:
+            return jsonify({'Erreur': 'Mauvaise valeur'}),500
+      else:
+        return jsonify({'Erreur': 'Mauvais attribut'}),500
+    else:
+      return jsonify({'Erreur': 'Requetes POST seulement'}),500
+    return jsonify({'Etat': json["etat"]}),200
+
+
+
+
+
+def Button_hold():
+    while True:
+        global ETAT
+        if(pi.read(BOUTON) == 0):
+            start = time.time()
+            while pi.read(BOUTON) == 0:
+                if(time.time() - start >= 2):
                     if(ETAT):
                         ETAT = False
+                        print("Programme mis en pause")
+                        break
                     else:
                         ETAT = True
-                    break
+                        print("Programme mis en start")
+                        break
                 else:
-                    break
+                    continue
+
+
+def Button_press():
+    while True:
+        global ETAT, TEMPERATURE, HUMIDITY
+        if (ETAT):
+            if(pi.read(BOUTON) == 0):
+                while True:
+                    if(pi.read(BOUTON) == 1):
+                        result = sensor.read()
+                        TEMPERATURE = str(result.get("temp_c"))
+                        HUMIDITY = str(result.get("humidity"))
+                        client.publish(TOPIC, TEMPERATURE)
+                        client.publish(TOPIC2, HUMIDITY)  
+                        print("Les infos ont été envoyées (button)")
+
+                        break
+        else:
+            continue
 
 
 #https://www.geeksforgeeks.org/how-to-create-a-countdown-timer-using-python/
 def countdown(t): 
     global ETAT
     while t: 
-        if(pi.read(BOUTON) == 0):
-            print("boutton cliquer")
-            while True:
-                if(pi.read(BOUTON) == 1):
-                    result = sensor.read()
-                    TEMPERATURE = str(result.get("temp_c"))
-                    HUMIDITY = str(result.get("humidity"))
-                    client.publish(TOPIC, TEMPERATURE)
-                    client.publish(TOPIC2, HUMIDITY)  
-                    break
-
         mins, secs = divmod(t, 60) 
         timer = '{:02d}:{:02d}'.format(mins, secs) 
         print(timer, end="\r") 
         sleep(1) 
         t -= 1
 
-while True:
-    try:
-        if ETAT == True:
-            countdown(10)
+def SendDataEach30sec():
+    while True:
+        global ETAT, TEMPERATURE, HUMIDITY
+        if (ETAT):
+            countdown(30)
+            result = sensor.read()
             TEMPERATURE = str(result.get("temp_c"))
             HUMIDITY = str(result.get("humidity"))
             client.publish(TOPIC, TEMPERATURE)
             client.publish(TOPIC2, HUMIDITY)  
-        else:
-            sendData()
-            continue
-    except TimeoutError:
-        print("Expired")
+            print("Les infos ont été envoyées (30sec)")
 
-                
+        else:
+            continue
+
+chrono = threading.Thread(target=SendDataEach30sec)   
+chrono.start()     
+
+hold_button = threading.Thread(target=Button_hold)
+hold_button.start()
+
+press_button = threading.Thread(target=Button_press)
+press_button.start()
             
-            
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=3000)
